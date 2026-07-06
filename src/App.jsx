0574a80f4ba06f52a1aef34b87b151f4ad0bda14
FelipeTagliabues <felipetagliabues.dev@outlook.com>
2026-07-06 20:30:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LogoPoupApp from "./assets/LogoPoupApp.svg"
+
+function App() {
+
+  return (
+    <>
+      <aside>
+        <img src={LogoPoupApp} alt="" />
+      </aside>
+    </>
+  )
+}
+
+export default App
