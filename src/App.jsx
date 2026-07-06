170f12fe57212eebddbcfab55f8d23a37fda6dbc
FelipeTagliabues <felipetagliabues.dev@outlook.com>
2026-07-06 20:47:13 -0300

    dev container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import LogoPoupApp from "./assets/LogoPoupApp.svg"
+import LogoPoupApp from "./assets/LogoPoupApp.svg";
+import { Container } from "./components/Container";
 
 function App() {
-
   return (
-    <>
+    <Container>
       <aside>
         <img src={LogoPoupApp} alt="" />
       </aside>
-    </>
-  )
+      <h3>Olá Mundo</h3>
+    </Container>
+  );
 }
 
-export default App
+export default App;
